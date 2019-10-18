c4e9535d27800de55ae449d8165f95f8f7d452d5
Dim04ka <suhotsky.dmitry@gmail.com>
2019-10-19 01:15:45 +0300

    add uslugi

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,43 @@ window.jQuery = require('jquery');
 var $ = require('jquery');
 var jQuery = require('jquery');
 
-$(document).ready(function() {
+$(function(){
+  var $page = $('html, body');
+  $('a[href*="#"]').click(function() {
+      $page.animate({
+          scrollTop: $($.attr(this, 'href')).offset().top
+      }, 400);
+      return false;
+  });
+})
 
-	
 
-	
-});
+//btn consultation
+$(function(){
+	$('.consult-btn').click(function(){
+		var $page = $('html, body');
+		
+				$page.animate({
+						scrollTop: $('.footer').offset().top
+				}, 400);
+				return false;
+	})
+})
+
+
+//burger mobile
+$(function(){
+	$('.header-burger').click(function(){
+		$('.header-menu').toggleClass('header-menu-active');
+
+		$('.header-menu-active a').click(function(){
+			$('.header-menu').removeClass('header-menu-active');
+		})
+	})
+})
+
+
+//
+$(function(){
+
+})
